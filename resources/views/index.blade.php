bcb4bcc603ce1f74cce64825d72f01e69b359791
skybedy <skybedy@gmail.com>
2022-10-13 11:42:09 +0200

    121022

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+
+@section('content')
+<div class="continer-fluid">
+    <h1>Běh Novojičínským parkem<br> 28.9.2023</h1>
+
+</div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
 Route::get('/', function () {
-    return view('welcome');
+   // return view('welcome');
 });
+*/
+
+
+Route::get('/',[HomeController::class,'index']);
